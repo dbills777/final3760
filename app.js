d7c6d3940096b85432342387da7aef0e5bfafdac
Derrick Bills <dbills777@gmail.com>
2020-12-15 11:13:35 -0700

    d

--- app.js
+++ app.js
@@ -12,31 +12,31 @@ mongoose.set('useFindAndModify', false);
 app.use(bodyParser.json());
 require('dotenv').config();
 
-mongoose
-  .connect(`${process.env.MONGOURL}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) =>
-    app.listen(process.env.PORT || port, () =>
-      console.log(`"Static Recipe App Listening at ${port}`)
-    )
-  )
-  .catch((err) => console.log(err));
 // mongoose
-//   .connect(
-//     'mongodb+srv://dgmFinal:dgmFinal123@cluster0.iorhc.mongodb.net/dgmFinal?retryWrites=true&w=majority',
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
+//   .connect(`${process.env.MONGOURL}`, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
 //   .then((result) =>
 //     app.listen(process.env.PORT || port, () =>
 //       console.log(`"Static Recipe App Listening at ${port}`)
 //     )
 //   )
 //   .catch((err) => console.log(err));
+mongoose
+  .connect(
+    'mongodb+srv://dgmFinal:dgmFinal123@cluster0.iorhc.mongodb.net/final?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((result) =>
+    app.listen(process.env.PORT || port, () =>
+      console.log(`"Static Recipe App Listening at ${port}`)
+    )
+  )
+  .catch((err) => console.log(err));
 
 //Set Veiw Engine
 app.set('view engine', 'ejs');
