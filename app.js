2910075d249641ef81407fb1710dea1fe7797735
Derrick Bills <dbills777@gmail.com>
2020-12-15 11:05:45 -0700

    .

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const mongoose = require('mongoose');
 const Rec = require('./recipeModel');
 const { result } = require('lodash');
 const app = express();
+app.use(express.static('public'));
+
 const port = 3000;
 mongoose.set('useFindAndModify', false);
 app.use(bodyParser.json());
 require('dotenv').config();
 
 mongoose
-  .connect('mongodb+srv://dgmFinal:<password>@cluster0.iorhc.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  .connect(`${process.env.MONGOURL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -21,6 +23,20 @@ mongoose
     )
   )
   .catch((err) => console.log(err));
+// mongoose
+//   .connect(
+//     'mongodb+srv://dgmFinal:dgmFinal123@cluster0.iorhc.mongodb.net/dgmFinal?retryWrites=true&w=majority',
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     }
+//   )
+//   .then((result) =>
+//     app.listen(process.env.PORT || port, () =>
+//       console.log(`"Static Recipe App Listening at ${port}`)
+//     )
+//   )
+//   .catch((err) => console.log(err));
 
 //Set Veiw Engine
 app.set('view engine', 'ejs');
